2f4f42325273cdd0412b4ee164df805eab4a64f0
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-07 23:15:10 -0300

    feat: Header, dicas e e letras

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import styles from "./app.module.css"
+
+import { Header } from './components/Header'
+import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
+
+function App() {
+
+  function handleRestartGame() {
+    alert("Reiniciar o jogo!")
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <main>
+          <Header current={5} max={10} onRestart={handleRestartGame} />
+          <Tip tip={`Uma das linguagens de programação mais utilizadas`} />
+          <div className={styles.word}>
+            <Letter value="R" />
+            <Letter value="E" />
+            <Letter value="A" />
+            <Letter value="C" />
+            <Letter value="T" />
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
